e81f72a6838402309fea445818bcb9ab3e170910
Bogus <bogus.frontend@gmail.com>
2022-10-21 03:03:33 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QRcode from 'react-qr-code';
+import './index.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="container text-center mx-auto w-2/4">Содержимое QR-кода</h1>
+      <input
+        type="text"
+        placeholder="Введите ссылку для генерации QR-кода"
+        className="border-gray-300 ml-35 w-full h-8 p-4 font-light text-sm focus:outline-none focus:border-gray-600"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <QRcode className="mx-auto mt-20" value={inputValue} />
     </div>
   );
 }
